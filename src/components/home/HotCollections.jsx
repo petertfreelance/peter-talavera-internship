b3b90a95ed8cbece96f6c4b52aa353689548bb07
Peter <petertfreelance@gmail.com>
2024-11-11 16:16:45 -0700

    hot collections update to call api and dynamically create carousel, as well as install carousel and axios functionality -Peter Talavera

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
+import ReactOwlCarousel from "react-owl-carousel";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
 
 const HotCollections = () => {
+
+  const [hotCollections, setHotCollections] = useState([]);
+
+  const state= {
+    responsive:{
+        0: {
+            items: 1,
+        },
+        450: {
+            items: 2,
+        },
+        600: {
+            items: 3,
+        },
+        1000: {
+            items: 4,
+        },
+    },
+}
+
+  async function getHotCollections() {
+    const apiData = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections');
+
+    setHotCollections(apiData.data);
+
+    
+
+  }
+
+
+  useEffect(() => {
+    getHotCollections();
+  }, []);
+
+  useEffect(() => {
+    console.log(hotCollections.length)
+  }, [hotCollections])
+
+
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,29 +56,58 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          <ReactOwlCarousel className="owl-theme hot-collections-carousel" loop nav margin={10} items={4} responsive={state.responsive}>
+          {hotCollections.length ? hotCollections.map((hotItem) => (
+            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12" key={hotItem.id}>
               <div className="nft_coll">
                 <div className="nft_wrap">
                   <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
+                    <img src={hotItem.nftImage} className="lazy img-fluid" alt="" />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
                   <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
+                    <img className="lazy pp-coll" src={hotItem.authorImage} alt="" />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{hotItem.title}</h4>
                   </Link>
-                  <span>ERC-192</span>
+                  <span>ERC-{hotItem.code}</span>
+                </div>
+              </div>
+            </div>
+          )): new Array(6).fill(0).map((_, index) => (
+            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12" key={index}>
+              <div className="nft_coll">
+                <div className="skeleton-box" style={{width: "100%", height: 200,}}>
+                  
+                </div>
+                <div className="nft_coll_pp">
+                  <Link to="/">
+                  <div className="skeleton-box" style={{width: 50, height: 50, borderRadius: "50%",}}>
+
+                  </div>
+                  </Link>
+                  <i className="fa fa-check"></i>
+                </div>
+                <div className="nft_coll_info">
+                  <Link to="/">
+                    <div className="skeleton-box" style={{width: 100, height: 20,}}>
+
+                    </div>
+                  </Link>
+                  <br />
+                  <div className="skeleton-box" style={{width: 60, height: 20,}}>
+                      
+                    </div>
                 </div>
               </div>
             </div>
           ))}
+          </ReactOwlCarousel>
         </div>
       </div>
     </section>
