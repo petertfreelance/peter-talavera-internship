a079b18b063aca151960c7e6bef1a9a5b9c6ee4c
Peter <petertfreelance@gmail.com>
2024-11-12 09:40:24 -0700

    top seller section api grab and dynamically place content - Peter Talavera

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -128,18 +128,10 @@ useEffect(() => {
           )):new Array(6).fill(0).map((_, index) => (
             <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12" key={index}>
               <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
+                <div className="author_list_pp skeleton-box" style={{width: 50, height: 50, borderRadius: "50%", display: "inline-block"}}>
+                  
                 </div>
-                <div className="de_countdown">5h 30m 32s</div>
+                <div className="de_countdown"></div>
 
                 <div className="nft__item_wrap">
                   <div className="nft__item_extra">
@@ -160,22 +152,14 @@ useEffect(() => {
                     </div>
                   </div>
 
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
+                  <div className="skeleton-box" style={{width: 200, height: 200}}></div>
                 </div>
                 <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
+                <div className="skeleton-box" style={{width: 100, height: 20,}}></div>
+                  <div className="nft__item_price"> ETH</div>
                   <div className="nft__item_like">
                     <i className="fa fa-heart"></i>
-                    <span>69</span>
+                    <span></span>
                   </div>
                 </div>
               </div>
